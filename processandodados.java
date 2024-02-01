4001c0109d855a4ee91d71327cd5b1031ceb069e
Diogo Pincetta <diogohe08@gmail.com>
2024-02-01 12:52:30 -0300

    Criação do projeto processando dados

---
+++ processandodados.java
@@ -0,0 +1,19 @@
+package processamento_dados_1;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String x;
+		x = sc.next();
+		System.out.println("Você diditou:" + x);
+		
+		
+		
+		sc.close();
+
+	}
+
+}
