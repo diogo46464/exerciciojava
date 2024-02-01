44f36acc801de440f1fbe89132e59eeaa68565e9
Diogo Pincetta <diogohe08@gmail.com>
2024-02-01 13:39:33 -0300

    Acrescentado Locale

--- processandodados.java
+++ processandodados.java
@@ -1,10 +1,11 @@
 package processamento_dados_1;
 
 import java.util.Scanner;
-
+import java.util.Locale;
 public class processandodados {
 
 		public static void main(String[] args) {
+			Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		double x;
 		x = sc.nextDouble();
