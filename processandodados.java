b8bbebf36f5299f4e895805139fe7c9bea756799
Diogo Pincetta <diogohe08@gmail.com>
2024-02-01 12:55:51 -0300

    Alteração de String para Int

--- processandodados.java
+++ processandodados.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String x;
+		int x;
 		x = sc.next();
 		System.out.println("Você diditou:" + x);
 		
