739b0df533b037354e3eb1411b6850f096ce3424
Diogo Pincetta <diogohe08@gmail.com>
2024-02-01 14:51:44 -0300

    Alteração para não ter quebra de linha pedente

--- quebradelinha.java
+++ quebradelinha.java
@@ -1,4 +1,4 @@
-package quebradelinha;
+package processamento_dados_1;
 
 import java.util.Scanner;
       public class Main{
@@ -6,10 +6,15 @@ import java.util.Scanner;
         	
          Scanner sc = new Scanner(System.in);
          String s1, s2, s3;
+         x = sc.nestLine();
+         sc = nextLine();
          s1 = sc.nextLine();
          s2 = sc.nextLine();
          s3 = sc.nextLine();
+
+
          System.out.println("DADOS DIGITADOS:");
+         System.out.println(x);
          System.out.println(s1);
          System.out.println(s2);
          System.out.println(s3);
